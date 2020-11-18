1dbf19b19c9aacec3cf6cfcfe98ef1eaeb4a996c
Jay Smith <61407072+jasonalexander-ja@users.noreply.github.com>
2020-11-18 02:10:38 +0000

    Completed graphing module, beginning refactoring the main page.

---
+++ react/freddo-index/src/graphing.js
@@ -0,0 +1,143 @@
+import React from 'react';
+
+function Lables(xyObjStats, xTextOffset, yTextOffset, className, xNum, yNum, width, height, xOffset, yOffset) {
+    const useableWidth = width - (2 * xOffset), useableHeight = height - (2 * yOffset);
+    const xIncrement = xyObjStats.xRange / xNum, yIncrement = xyObjStats.yRange / yNum;
+    const xPosIncrement = useableWidth / xNum, yPosIncrement = useableHeight / yNum;
+    let labels = [];
+    for(let i = 0; i <= xNum; i++) {
+        let xPos = i * xPosIncrement;
+        let text = parseFloat((xyObjStats.xMin + i * xIncrement).toFixed(0)).toString();
+        labels.push(<text x={xOffset + xPos} y={height - xTextOffset} className={className} >{text}</text>);
+    }
+    for(let i = 0; i <= yNum; i++) {
+        let yPos = i * yPosIncrement;
+        let text = parseFloat((xyObjStats.yMin + i * yIncrement).toFixed(2)).toString();
+        labels.push(<text x={yTextOffset} y={height - yPos - yOffset} className={className} >{text}</text>);
+    }
+    return labels;
+}
+
+export class Graph extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            graphHeight: 
+                this.props.graphHeight ? this.props.graphHeight : 220,
+            graphWidth: 
+                this.props.graphWidth ? this.props.graphWidth : 400,
+            textClassName: 
+                this.props.className ? this.props.textClassName : "",
+            lineClassName: 
+                this.props.lineClassName ? this.props.lineClassName : "",
+            xTextOffset:
+                this.props.xTextOffset ? this.props.xTextOffset : 2,
+            yTextOffset:
+                this.props.yTextOffset ? this.props.yTextOffset : 2,
+            xLabels:
+                this.props.numXLabels ? this.props.numXLabels : 2,
+            yLabels:
+                this.props.numYLabels ? this.props.numYLabels : 2,
+        }
+    }
+
+
+    render() {
+
+        // Validating data, getting default values
+
+        if(!isDatasetValid(this.props.graphData)) 
+            return <p>Data set needs to contain at least 1 {"{{x: 0, y: 0}}"} array entry</p>;
+        const xOffset = 
+            this.props.xOffset ? this.props.xOffset : 35;
+        const yOffset = 
+            this.props.yOffset ? this.props.xOffset : 35;
+
+        const xyStats = 
+            getXYStats(this.props.graphData);
+        const xyCoordinates = 
+            getDisplayData(this.props.graphData, 
+                xyStats, 
+                this.state.graphWidth, 
+                this.state.graphHeight, 
+                xOffset,
+                yOffset,
+            );
+        const labels = Lables(xyStats, 
+            this.state.xTextOffset, 
+            this.state.yTextOffset, 
+            this.state.textClassName,
+            this.state.xLabels,
+            this.state.yLabels,
+            this.state.graphWidth,
+            this.state.graphHeight,
+            xOffset,
+            yOffset
+        );
+        let polylinePoints =  "";
+        xyCoordinates.forEach(val => {
+            polylinePoints += `${val.x},${val.y} `;
+        });
+        return (
+            <div>
+                <svg
+                    width={`${this.props.graphWidth}`}
+                    height={`${this.props.graphHeight}`}
+                    className={this.props.className}
+                >
+                    <polyline 
+                        points={polylinePoints} 
+                        className={this.state.lineClassName}
+                    />
+                    {labels}
+                </svg>
+            </div>
+        )
+    }
+}
+
+function isDatasetValid(xyObjArr) {
+    return ('x' in xyObjArr[0]) && ('y' in xyObjArr[0]);  
+}
+
+function getXYStats(xyObjArr) {
+    let dataObj = xyObjArr.map(val => { return {x: val.x, y: parseFloat(val.y)} });
+    let xMin = xyObjArr[0].x, xMax = 0;
+    let yMin = dataObj[0].y, yMax = 0;
+    dataObj.forEach(val => { 
+        xMin = xMin > val.x ? val.x : xMin;
+        xMax = xMax < val.x ? val.x : xMax;
+        yMin = yMin > val.y ? val.y : yMin;
+        yMax = yMax < val.y ? val.y : yMax;
+     });
+     const xRange = xMax - xMin;
+     const yRange = (yMax - yMin).toFixed(2);
+     return {
+        xMin: xMin,
+        yMin: yMin,
+        xMax: xMax,
+        yMax: yMax,
+        xRange: xRange,
+        yRange: yRange,
+     }
+}
+
+function getDisplayData(xyObjArr, xyObjstats, w, h, xNegOffset, yNegOffset) {
+    const height = h - (yNegOffset * 2), width = w - (xNegOffset * 2);
+    let dataObj = xyObjArr.map(val => { return {x: val.x, y: parseFloat(val.y)} });
+    let xMin = xyObjstats.xMin;
+    let yMin = xyObjstats.yMin;
+     const xRange = xyObjstats.xRange;
+     const yRange = xyObjstats.yRange;
+     const xMultiplier = width  / xRange, xOffset = 0 - xMin;
+     const yMultiplier = height / yRange, yOffset = 0 - yMin;
+     let convertedArr = dataObj.map(val => {
+         return {
+            x: Math.floor((val.x + xOffset) * xMultiplier) + xNegOffset,
+            y: height - Math.floor((val.y + yOffset) * yMultiplier) + yNegOffset
+        }
+     });
+     return convertedArr;
+}
+
+export default { Graph };
--- react/freddo-index/src/index.js
+++ react/freddo-index/src/index.js
@@ -1,109 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Graph } from './test_modul.js'
-
-class KPI extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            psDataset: [],
-        }
-    }
-
-    makeDataset() {
-        const changes = this.props.changeObj;
-        const dateRange = new Date().getFullYear() - new Date(changes[0].x).getFullYear();
-        const startYear = new Date(changes[0].x).getFullYear();
-        const currency = this.props.currency;
-        let psDataset = [];
-        for(let i = 0; i <= dateRange; i++) {
-            psDataset.push({
-                x: new Date(changes[0].x).getFullYear() + i, 
-                y: getPriceOnYear(changes, startYear + i)
-            });
-        }
-        let convertedDataset = [];
-        psDataset.forEach((val, i) => {
-            const rate = this.props.periodExchangeObj[startYear + i][currency]; 
-            if(rate)
-                convertedDataset.push({x: val.x, y: (val.y * rate).toFixed(2)})
-        });
-        return convertedDataset;
-    }
-
-    makeLables(xMax, yMax, xMin, yMin, xRange, yRange, height, width, offset, xDec, yDec) {
-        const useableWidth = width - (2 * offset), useableHeight = height - (2* offset);
-        const xIncrement = xRange / xMax, yIncrement = yRange / yMax;
-        const xPosIncrement = useableWidth / xMax, yPosIncrement = useableHeight / yMax;
-        let xLabels = [], yLables = [], xLength = 0, yLength = 0;
-        for(let i = 0; i <= xMax; i++) {
-            let xPos = offset + i * xPosIncrement;
-            let text = parseFloat((xMin + i * xIncrement).toFixed(xDec)).toString();
-            xLength = xLength < text.length ? text.length : xLength;
-        }
-        for(let i = 0; i <= yMax; i++) {
-            let yPos = offset + i * yPosIncrement;
-            let text = parseFloat((yMin + i * yIncrement).toFixed(yDec)).toString();
-            yLength = yLength < text.length ? text.length : yLength;
-        }
-    }
-
-    render() {
-        const kpiHeight = 220, kpiWidth = 400, offset = 35;
-        const displayData = getDisplayData(this.makeDataset(), kpiWidth, kpiHeight, offset, offset);
-        console.log(this.makeDataset());
-        const xyCoordinates = displayData.convertedArr;
-        let polylinePoints =  "";
-        xyCoordinates.forEach(val => {
-            polylinePoints += `${val.x},${val.y} `;
-        });
-        return(
-            <div>
-                <svg 
-                    className="mainKpi"
-                    height={`${kpiHeight}px`} 
-                    width={`${kpiWidth}px`}
-                >
-                    <polyline 
-                        points={polylinePoints} 
-                        className="line"
-                    />
-                    <text 
-                        x={kpiWidth / 2} 
-                        y={kpiHeight - 5} 
-                        fontSize="15px"
-                    fontFamily="arial">{displayData.xMin + Math.floor(displayData.xRange/2)}</text>
-                    <text 
-                        x={kpiWidth - offset} 
-                        y={kpiHeight - 5} 
-                        fontSize="15px"
-                    fontFamily="arial">{displayData.xMax}</text>
-                    <text 
-                        x={offset} 
-                        y={kpiHeight - 5} 
-                        fontSize="15px"
-                    fontFamily="arial">{displayData.xMin}</text>
-                    <text 
-                        x={5} 
-                        y={kpiHeight - offset} 
-                        fontSize="15px"
-                    fontFamily="arial">{displayData.yMin}</text>
-                    <text 
-                        x={5} 
-                        y={kpiHeight - (kpiHeight/2)} 
-                        fontSize="15px"
-                    fontFamily="arial">{parseFloat((displayData.yMin + (displayData.yRange/2)).toFixed(2))}</text>
-                    <text 
-                        x={5} 
-                        y={offset} 
-                        fontSize="15px"
-                    fontFamily="arial">{displayData.yMax}</text>
-                </svg>
-            </div>
-        )
-    }
-}
+import { Graph } from './graphing.js'
 
 class Main extends React.Component {
     constructor(props) {
@@ -119,34 +17,32 @@ class Main extends React.Component {
         };
     }
     updateYear(year) {
-        fetchJSON(`https://api.exchangeratesapi.io/${year}-01-01?base=GBP`)
-            .then(data => {
-                if(data.rates[this.state.currency])
-                    this.setState({
-                        year: year,
-                        costPs: getPriceOnYear(this.props.changeObj, year),
-                        exchangeRates: data.rates,
-                        exchangeRate: data.rates[this.state.currency],
-                        error: false,
-                        errorMsg: "",
-                    });
-                else {
-                    let curr = "";
-                    let code = this.state.currency;
-                    this.props.currencyOptions.forEach(obj => { if(obj.code === code) curr = obj.value; });
-                    this.setState({
-                        error: true,
-                        errorMsg: `No data for ${curr} for ${year}.`,
-                    })
-                }
+        const conversionObj = this.props.periodExchangeObj[`${year}`];
+        if(!conversionObj) {
+            this.setState({
+                error: true,
+                errorMsg: `Couldn't load exchange rates for year ${year}`,
             })
-            .catch(e => { 
-                console.log("Failed to get conversion object."); 
-                this.setState({
-                    error: true,
-                    errorMsg: `Couldn't load exchange rates for year ${year}`,
-                })
+            return;
+        }
+        if(!conversionObj[this.state.currency]) {
+            let curr = "";
+            let code = this.state.currency;
+            this.props.currencyOptions.forEach(obj => { if(obj.code === code) curr = obj.value; });
+            this.setState({
+                error: true,
+                errorMsg: `No data for ${curr} for ${year}.`,
             });
+            return;
+        }
+        this.setState({
+            year: year,
+            costPs: getPriceOnYear(this.props.changeObj, year),
+            exchangeRates: conversionObj,
+            exchangeRate: conversionObj[this.state.currency],
+            error: false,
+            errorMsg: "",
+        });
     }
     updateCurrency(val) {
         if(this.state.exchangeRates[val])
@@ -182,7 +78,10 @@ class Main extends React.Component {
         psDataset.forEach((val, i) => {
             const rate = this.props.periodExchangeObj[startYear + i][currency]; 
             if(rate)
-                convertedDataset.push({x: val.x, y: (val.y * rate).toFixed(2)});
+                convertedDataset.push({
+                    x: val.x, 
+                    y: parseFloat((val.y * rate).toFixed(2))
+                });
         });
         return convertedDataset;
     }
@@ -220,16 +119,15 @@ class Main extends React.Component {
                 >{currencyOptions}</select>&nbsp;&nbsp;
                 <nobr className="text">{(this.state.costPs * this.state.exchangeRate).toFixed(2)}</nobr>&nbsp;&nbsp;
                 <nobr className="errorText">{this.state.errorMsg}</nobr>
-                <KPI 
-                    changeObj={this.props.changeObj}
-                    currency={this.state.currency}
-                    periodExchangeObj={this.props.periodExchangeObj}
-                />
                 <Graph 
                     className="mainKpi" 
                     graphWidth={400}
                     graphHeight={220}
                     graphData={graphData}
+                    textClassName="kpiText"
+                    xTextOffset={5}
+                    yTextOffset={5}
+                    lineClassName="line"
                 />
             </div>
         )
@@ -238,37 +136,6 @@ class Main extends React.Component {
 
 // General helper functions
 
-function getDisplayData(Obj, w, h, offset) {
-    const height = h - (offset * 2), width = w - (offset * 2);
-    let dataObj = Obj.map(val => { return {x: val.x, y: parseFloat(val.y)} });
-    let xMin = dataObj[0].x, xMax = 0;
-    let yMin = dataObj[0].y, yMax = 0;
-    dataObj.forEach(val => { 
-        xMin = xMin > val.x ? val.x : xMin;
-        xMax = xMax < val.x ? val.x : xMax;
-        yMin = yMin > val.y ? val.y : yMin;
-        yMax = yMax < val.y ? val.y : yMax;
-     });
-     const xRange = xMax - xMin;
-     const yRange = (yMax - yMin).toFixed(2);
-     const xMultiplier = width  / xRange, xOffset = 0 - xMin;
-     const yMultiplier = height / yRange, yOffset = 0 - yMin;
-     let convertedArr = dataObj.map(val => {
-         return {
-            x: Math.floor((val.x + xOffset) * xMultiplier) + offset,
-            y: height - Math.floor((val.y + yOffset) * yMultiplier) + offset
-        }
-     });
-     return  {
-        xMin: xMin,
-        yMin: yMin,
-        xMax: xMax,
-        yMax: yMax,
-        xRange: xRange,
-        yRange: yRange,
-        convertedArr: convertedArr,
-    };
-}
 function generateDateOptions(list, periodExchangeObj, currency) {
     let optionList = [];
     let years = list.map(val => new Date(val.x).getFullYear());
